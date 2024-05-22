415790940c30ed27c35c4dc2fbce3d5293274ceb
EgorenkoDN <egorenkodn@psbank.ru>
2024-05-22 21:34:28 +0300

    feat: add contrast

--- src/components/color-picker/color-picker.component.ts
+++ src/components/color-picker/color-picker.component.ts
@@ -21,4 +21,28 @@ export class ColorPickerComponent {
   handleClose(): void {
     this.close.emit();
   }
+
+  calculateContrast(): number {
+    const luminance = (r: number, g: number, b: number) => {
+      const a = [r, g, b].map((v) => {
+        v /= 255;
+        return v <= 0.03928 ? v / 12.92 : Math.pow((v + 0.055) / 1.055, 2.4);
+      });
+      return a[0] * 0.2126 + a[1] * 0.7152 + a[2] * 0.0722;
+    };
+
+    const lum1 = luminance(
+      this.color1!.pixel.r,
+      this.color1!.pixel.g,
+      this.color1!.pixel.b
+    );
+    const lum2 = luminance(
+      this.color2!.pixel.r,
+      this.color2!.pixel.g,
+      this.color2!.pixel.b
+    );
+    const brightest = Math.max(lum1, lum2);
+    const darkest = Math.min(lum1, lum2);
+    return (brightest + 0.05) / (darkest + 0.05);
+  }
 }
